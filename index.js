0a3352d620b95c829a85a241ffd95aca287d0c1f
gustavo <gustavo.olmedo.formosa@gmail.com>
2020-10-08 21:16:31 -0300

    first version

---
+++ index.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+console.log(`
+Hello,
+
+I am a sr. Javascript developer. I have been developing websites and applications for around 7 years. Most my experience lies in full stack development. I have worked with HTML/ CSS, Javascript (NodeJS-ReactJS), .NET, and a little bit of Python.
+
+I enjoy being challenged and engaging with projects that require me to work outside my comfort and knowledge set, as continuing to learn new languages and development techniques are important to me.
+
+Sincerely,
+Gustavo Adolfo Olmedo
+
+Linkedin: https://www.linkedin.com/in/gustavo-olmedo/
+E-mail: gustavo.olmedo.formosa@gmail.com
+
+`);
