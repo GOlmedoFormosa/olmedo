fe24d21e6087a2a33bb7acc33c0aa5d92abc7700
gustavo <gustavo.olmedo.formosa@gmail.com>
2020-10-08 22:34:10 -0300

    add colors

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
 #!/usr/bin/env node
 
 const clearConsole = require('clear-any-console');
+const chalk = require('chalk');
+const log = console.log;
 
 // Clears the console.
 clearConsole();
 
-console.log(`
-Hello,
+const styleText = (str) => chalk.green.bgBlack.bold(str);
+const styleInfo = (str) => chalk.white.bold.italic.underline(str);
 
-I am a sr. Javascript developer. I have been developing websites and applications for around 7 years. Most my experience lies in full stack development. I have worked with HTML/ CSS, Javascript (NodeJS-ReactJS), .NET, and a little bit of Python.
+log(`
+${styleText(' Hello, ')}
 
-I enjoy being challenged and engaging with projects that require me to work outside my comfort and knowledge set, as continuing to learn new languages and development techniques are important to me.
+${styleText(' My name is Gustavo and I am a sr. Javascript developer. I have been developing websites and applications for around 7 years. Most my experience lies in full stack development. I have worked with HTML/ CSS, Javascript (NodeJS-ReactJS), .NET, and a little bit of Python. ')}
 
-Sincerely,
-Gustavo Adolfo Olmedo
+${styleText(' I enjoy being challenged and engaging with projects that require me to work outside my comfort and knowledge set, as continuing to learn new languages and development techniques are important to me. ')}
 
-Linkedin: https://www.linkedin.com/in/gustavo-olmedo/
-E-mail: gustavo.olmedo.formosa@gmail.com
+${styleText(' Sincerely, ')}
+${styleText(' Gustavo Adolfo Olmedo ')}
+
+${styleText('Linkedin:')} ${styleInfo('https://www.linkedin.com/in/gustavo-olmedo/')}
+${styleText('E-mail:')} ${styleInfo('gustavo.olmedo.formosa@gmail.com')}
 
 `);
